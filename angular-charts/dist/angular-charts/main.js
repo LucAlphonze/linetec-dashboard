89db9432c8056e87c373aff9e8df33a15d34c59b
Lucas Alfonzo <lucasalfonzo19@gmail.com>
2023-11-03 10:45:39 -0300

    urls-fixed

--- angular-charts/dist/angular-charts/main.js
+++ angular-charts/dist/angular-charts/main.js
@@ -5031,21 +5031,21 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony export */ });
 const environment = {
   production: false,
-  API_URL_USERS: 'http://automatismopalmar-dev.ddns.net:3001/api/users/',
-  API_URL_ROLES: 'http://automatismopalmar-dev.ddns.net:3001/api/role/',
-  API_URL_RGENERAL: 'http://automatismopalmar-dev.ddns.net:3001/api/registro-general//',
-  API_URL_VARIABLES: 'http://automatismopalmar-dev.ddns.net:3001/api/variables/',
-  API_URL_PAISES: 'http://automatismopalmar-dev.ddns.net:3001/api/paises/',
-  API_URL_PROVINCIAS: 'http://automatismopalmar-dev.ddns.net:3001/api/provincias/',
-  API_URL_LOCALIDADES: 'http://automatismopalmar-dev.ddns.net:3001/api/localidad/',
-  API_URL_EMPRESAS: 'http://automatismopalmar-dev.ddns.net:3001/api/empresas/',
-  API_URL_PLANTA: 'http://automatismopalmar-dev.ddns.net:3001/api/empresa-planta/',
-  API_URL_TIPO_MAQUINA: 'http://automatismopalmar-dev.ddns.net:3001/api/tipo-maquina/',
-  API_URL_MAQUINA: 'http://automatismopalmar-dev.ddns.net:3001/api/maquinas/',
-  API_URL_LINEA_PRODUCCION: 'http://automatismopalmar-dev.ddns.net:3001/api/linea-produccion/',
-  API_URL_PROCESO: 'http://automatismopalmar-dev.ddns.net:3001/api/proceso/',
-  API_URL_TRIGGER: 'http:///automatismopalmar-dev.ddns.net:3001/api/trigger/',
-  API_URL_RTOKEN: 'http:///automatismopalmar-dev.ddns.net:3001/api/refresh'
+  API_URL_USERS: 'http://automatismopalmar.ddns.net:3001/api/users/',
+  API_URL_ROLES: 'http://automatismopalmar.ddns.net:3001/api/role/',
+  API_URL_RGENERAL: 'http://automatismopalmar.ddns.net:3001/api/registro-general//',
+  API_URL_VARIABLES: 'http://automatismopalmar.ddns.net:3001/api/variables/',
+  API_URL_PAISES: 'http://automatismopalmar.ddns.net:3001/api/paises/',
+  API_URL_PROVINCIAS: 'http://automatismopalmar.ddns.net:3001/api/provincias/',
+  API_URL_LOCALIDADES: 'http://automatismopalmar.ddns.net:3001/api/localidad/',
+  API_URL_EMPRESAS: 'http://automatismopalmar.ddns.net:3001/api/empresas/',
+  API_URL_PLANTA: 'http://automatismopalmar.ddns.net:3001/api/empresa-planta/',
+  API_URL_TIPO_MAQUINA: 'http://automatismopalmar.ddns.net:3001/api/tipo-maquina/',
+  API_URL_MAQUINA: 'http://automatismopalmar.ddns.net:3001/api/maquinas/',
+  API_URL_LINEA_PRODUCCION: 'http://automatismopalmar.ddns.net:3001/api/linea-produccion/',
+  API_URL_PROCESO: 'http://automatismopalmar.ddns.net:3001/api/proceso/',
+  API_URL_TRIGGER: 'http:///automatismopalmar.ddns.net:3001/api/trigger/',
+  API_URL_RTOKEN: 'http:///automatismopalmar.ddns.net:3001/api/refresh'
 };
 
 /***/ }),
