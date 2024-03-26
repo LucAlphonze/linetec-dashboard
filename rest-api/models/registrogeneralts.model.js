1df78c0c71144cd33621aaa6dfa5274c3530f3e4
Lucas Alfonzo <lucasalfonzo19@gmail.com>
2024-03-26 07:36:14 -0300

    registroGeneralts.model-added-timeseries-option

--- rest-api/models/registrogeneralts.model.js
+++ rest-api/models/registrogeneralts.model.js
@@ -1,30 +1,40 @@
 const { Schema, model } = require("mongoose");
 
-const RegistroGeneralTSSchema = Schema({
-  fecha_lectura: {
-    type: Date,
-    required: true,
-  },
-  metaData: [
-    {
-      datos: {
-        type: Number,
-        required: true,
-      },
-      id_variable: {
-        type: Schema.Types.ObjectId,
-        ref: "Variable",
-      },
-      _id: false,
+const RegistroGeneralTSSchema = Schema(
+  {
+    fecha_lectura: {
+      type: Date,
+      required: true,
     },
-  ],
-  modo: {
-    type: String,
-    default: "automatica",
+    metaData: [
+      {
+        datos: {
+          type: Number,
+          required: true,
+        },
+        id_variable: {
+          type: Schema.Types.ObjectId,
+          ref: "Variable",
+        },
+        _id: false,
+      },
+    ],
+    modo: {
+      type: String,
+      default: "automatica",
+    },
+    time_stamp: {
+      type: Date,
+    },
   },
-  time_stamp: {
-    type: Date,
-  },
-});
+  {
+    timeseries: {
+      timeField: fecha_lectura,
+      metaField: metaData,
+      granularity: "seconds",
+    },
+  }
+);
+RegistroGeneralTSSchema.index({ metaData: 1, fecha_lectura: -1 });
 
 module.exports = model("RegistroGeneralts", RegistroGeneralTSSchema);
